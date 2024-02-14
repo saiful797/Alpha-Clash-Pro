ddbb019dbe105ec802ab744c0fa5bb705ec0970f
saiful797 <saifulislamsaif797@gmail.com>
2024-02-14 16:20:16 +0600

    added classlist

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -1,3 +1,9 @@
 function play(){
-    console.log('Play Start Now');
+    //step-1: Hide the home screen. to hide the screen add and the class hidden to the home screen
+    const homeScreen = document.getElementById('home-screen');
+    // console.log(homeScreen.classList);
+    homeScreen.classList.add('hidden');
+
+    
+    //step-2; Show the playground
 }
