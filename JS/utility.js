bb63f45133ead2ebc13ea438d25c104e7d1cd5f3
saiful797 <saifulislamsaif797@gmail.com>
2024-02-14 17:07:02 +0600

    added function

--- JS/utility.js
+++ JS/utility.js
@@ -7,3 +7,15 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function GetARandomAlphabet(){
+    //get or create alphabet array
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabet = alphabetString.split('');
+    console.log(alphabet);
+
+    //get a random index 0 to 25
+    const randomNumber = math.random() * 25;
+    const index = math.round(randomNumber);
+    console.log(index);
+}
