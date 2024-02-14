ce73d1295ce2c7cd9f8818f50af77c164c7a1409
saiful797 <saifulislamsaif797@gmail.com>
2024-02-14 16:26:37 +0600

    added playGroundSection.classList.remove('hidden')

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -1,9 +1,12 @@
 function play(){
     //step-1: Hide the home screen. to hide the screen add and the class hidden to the home screen
     const homeScreen = document.getElementById('home-screen');
-    // console.log(homeScreen.classList);
     homeScreen.classList.add('hidden');
+    // console.log(homeScreen.classList);
 
-    
-    //step-2; Show the playground
+
+    //step-2: Show the playground
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
+    // console.log(playGroundSection.classList);
 }
