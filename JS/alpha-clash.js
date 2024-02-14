963e9a6abdf9acc980c1cf002711be0855ece2f7
saiful797 <saifulislamsaif797@gmail.com>
2024-02-14 16:54:37 +0600

    added utility.js file

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -1,12 +1,22 @@
+// function play(){
+//     //step-1: Hide the home screen. to hide the screen add and the class hidden to the home screen
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//     // console.log(homeScreen.classList);
+
+
+//     //step-2: Show the playground
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+//     // console.log(playGroundSection.classList);
+// }
+
+function continueGame(){
+    //step-1: generate a random Alphabet
+}
+
 function play(){
-    //step-1: Hide the home screen. to hide the screen add and the class hidden to the home screen
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    // console.log(homeScreen.classList);
-
-
-    //step-2: Show the playground
-    const playGroundSection = document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden');
-    // console.log(playGroundSection.classList);
+    hiddenElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
