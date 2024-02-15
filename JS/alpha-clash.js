dd2916a72d84e1eaecff1ed08e86eb53e8c17201
saiful797 <saifulislamsaif797@gmail.com>
2024-02-15 11:36:39 +0600

    added utility.js

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -18,7 +18,53 @@ function handleKeyboardButtonPress(event){
 
     //get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
-    console.log(currentAlphabetElement.innerText);
+    const currentAlphabet = currentAlphabetElement.innerText;
+    
+    console.log(playerPressed, currentAlphabet.toLocaleLowerCase());
+    //console.log(currentAlphabetElement.innerText);
+
+    //check matched or not
+    if(playerPressed === currentAlphabet.toLocaleLowerCase()){
+        console.log('You get a pont');
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+
+        setTextElementValueById('current-score', updatedScore);
+
+        // -----------------------------------------------------------------
+        // //Update score
+        // // 1.get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // //console.log(currentScore);
+
+        // // 2.increase the score by 1
+        // const newScore = currentScore + 1;
+        // // 3.show the updated score
+        // currentScoreElement.innerText = newScore;
+        // //start a new round
+        removeBackgroundColorById(playerPressed);
+        continueGame();
+    }
+    else{
+        console.log('You missed. You lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life',updatedLife);
+
+
+        // -----------------------------------------------------------
+        // //Step-1: Get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // //step-2: reduce the life count
+        // const newLife = currentLife - 1;
+        // //step-3: display the updated life count
+        // currentLifeElement.innerText = newLife;
+    }
 }
 //capture keyboard key press
 document.addEventListener('keyup', handleKeyboardButtonPress);
